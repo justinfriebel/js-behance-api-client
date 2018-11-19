896b932801d8fe6fd4af8b769e2d70cf3a4e7cda
Justin Friebel <friebj99@gmail.com>
2018-11-18 23:18:03 -0600

    ProfileExperience tests.

--- src/components/__tests__/ProfileExperience.test.js
+++ src/components/__tests__/ProfileExperience.test.js
@@ -1,8 +1,44 @@
 import React from "react";
 import { shallow } from "enzyme";
 import ProfileExperience from "../profile/ProfileExperience";
-import user from "../__mocks__/user";
+import experience from "../__mocks__/experience.json";
+import user from "../__mocks__/user.json";
 
-it("renders ProfileExperience without crashing", () => {
-  shallow(<ProfileExperience user={user} />);
+describe("ProfileExperience", () => {
+  const profileExperience = shallow(<ProfileExperience user={user} />);
+
+  beforeAll(() => {
+    profileExperience.setState({ experience: experience });
+  });
+
+  afterAll(() => {
+    profileExperience.setState({ experience: null });
+  });
+
+  it("renders the first experience", () => {
+    expect(
+      profileExperience
+        .find("p")
+        .at(0)
+        .text()
+    ).toEqual("Art Director, Designer at Peter Tarka");
+  });
+
+  it("renders the second experience", () => {
+    expect(
+      profileExperience
+        .find("p")
+        .at(1)
+        .text()
+    ).toEqual("Designer at JUICE");
+  });
+
+  it("renders the third experience", () => {
+    expect(
+      profileExperience
+        .find("p")
+        .at(2)
+        .text()
+    ).toEqual("Designer at JWT (J. Walter Thompson)");
+  });
 });
--- src/components/__tests__/ProfileImage.test.js
+++ src/components/__tests__/ProfileImage.test.js
@@ -4,12 +4,14 @@ import React from "react";
 import ProfileImage from "../profile/ProfileImage";
 import user from "../__mocks__/user";
 
-it("is not null", () => {
-  const { getByAltText } = render(<ProfileImage user={user} />);
-  expect(getByAltText(user.display_name)).not.toBeNull();
-});
+describe("ProfileImage", () => {
+  it("is not null", () => {
+    const { getByAltText } = render(<ProfileImage user={user} />);
+    expect(getByAltText(user.display_name)).not.toBeNull();
+  });
 
-it("renders the image", () => {
-  const { getByAltText } = render(<ProfileImage user={user} />);
-  expect(getByAltText(user.display_name)).toBeInTheDocument();
+  it("renders the image", () => {
+    const { getByAltText } = render(<ProfileImage user={user} />);
+    expect(getByAltText(user.display_name)).toBeInTheDocument();
+  });
 });

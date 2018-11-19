60bdf44ac7bf9f06dc87ea280eda1e8660f82017
Justin Friebel <friebj99@gmail.com>
2018-11-19 12:46:22 -0600

    SVG not working on codesandbox for some reason ¯\_(ツ)_/¯.

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,7 +1,7 @@
 import React from "react";
 import AsyncSelect from "react-select/lib/Async";
 import styled from "styled-components";
-import logo from "../images/logo.svg";
+import logo from "../images/logo.png";
 import { Box, Flex, Image } from "rebass";
 
 const SearchInput = ({ onInputChange, loadOptions, onChange, value }) => {
